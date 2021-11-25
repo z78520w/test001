f099b8a710c5f117f915ca67e76e4c9c0a8bbae1
james986g <87516799+james986g@users.noreply.github.com>
2021-11-25 12:34:00 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 
-# Download and install xray
-mkdir /tmp/xray
-curl -L -H "Cache-Control: no-cache" -o /tmp/xray/xray.zip https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip
-unzip /tmp/xray/xray.zip -d /tmp/xray
-install -m 755 /tmp/xray/xray /usr/local/bin/xray
+# Global variables
+DIR_CONFIG="/etc/v2ray"
+DIR_RUNTIME="/usr/bin"
+DIR_TMP="$(mktemp -d)"
 
-# Remove temporary directory
-rm -rf /tmp/xray
-
-# xray new configuration
-install -d /usr/local/etc/xray
-cat << EOF > /usr/local/etc/xray/config.json
-{
+# Write V2Ray configuration
+cat << EOF > ${DIR_TMP}/heroku.json
+{{
     "inbounds": [
         {
             "port": 443,
@@ -51,5 +46,19 @@ cat << EOF > /usr/local/etc/xray/config.json
         }
     ]
 }
-# Run xray
-/usr/local/bin/xray -config /usr/local/etc/xray/config.json
+EOF
+
+# Get V2Ray executable release
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
+busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
+
+# Convert to protobuf format configuration
+mkdir -p ${DIR_CONFIG}
+${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
+
+# Install V2Ray
+install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
+rm -rf ${DIR_TMP}
+
+# Run V2Ray
+${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
