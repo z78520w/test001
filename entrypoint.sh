2ceb2a47e225ab546161e8586870a2aed022ca54
james986g <87516799+james986g@users.noreply.github.com>
2021-11-26 10:17:55 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,31 +13,62 @@ rm -rf /tmp/xray
 install -d /usr/local/etc/xray
 cat << EOF > /usr/local/etc/xray/config.json
 {
-  "inbounds": [
-    {
-      "port": 443,
-      "protocol": "vmess",
-      "settings": {
-        "clients": [
-          {
-            "id": "76974d1a-cbd6-4b6f-db1d-38d78b3fb169"
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": "ws",
-        "wsSettings": {
-        "path": "/postsray"
+    "inbounds": [{
+        "port": 443,
+        "protocol": "vmess",
+        "settings": {
+            "clients": [{
+                "id": "18a28d40-fb08-942f-a920-01a34c1d608d",
+                "level": 1,
+                "alterId": 64
+            }]
+        },
+        "streamSettings": {
+            "network": "tcp",
+            "tcpSettings": {
+                "header": {
+                    "type": "http",
+                    "response": {
+                        "version": "1.1",
+                        "status": "200",
+                        "reason": "OK",
+                        "headers": {
+                            "Content-Type": ["application/octet-stream",
+                            "application/x-msdownload",
+                            "text/html",
+                            "application/x-shockwave-flash"],
+                            "Transfer-Encoding": ["chunked"],
+                            "Connection": {
+                            "Pragma": "no-cache"
+                        }
+                    }
+                }
+            }
         }
-      }
-    }
-  ],
-  "outbounds": [
+    }],
+    "outbounds": [{
+        "protocol": "freedom",
+        "settings": {
+ 
+        }
+    },
     {
-      "protocol": "freedom",
-      "settings": {}
+        "protocol": "blackhole",
+        "settings": {
+ 
+        },
+        "tag": "blocked"
+    }],
+    "routing": {
+        "strategy": "rules",
+        "settings": {
+            "rules": [{
+                "type": "field",
+                "ip": ["geoip:private"],
+                "outboundTag": "blocked"
+            }]
+        }
     }
-  ]
 }
 EOF
 
